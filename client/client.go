8dfbb893a9b039232d1e3f61d94a5d525f5fd221
Apostolos Tselios <apostolis.tselios@gmail.com>
2019-10-26 18:20:13 +0300

    Add getCredentials function to client.go.
    getCredentials gets from the user the username and password and returns
    them.

--- client/client.go
+++ client/client.go
@@ -14,11 +14,34 @@ func main() {
 	}
 	defer conn.Close()
 
-	fmt.Fprintf(conn, "hello\n")
+	fmt.Println("Welcome to the ATM.")
+	username, password, err := getCredentials()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(username, password)
 
 	response, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	fmt.Println(response)
+}
+
+// getCredentials gets from the user the username and password and returns them.
+func getCredentials() (string, string, error) {
+	var username string
+	var password string
+	fmt.Print("Username: ")
+	_, err := fmt.Scanf("%s\n", &username)
+	if err != nil {
+		return "", "", err
+	}
+	fmt.Print("Password: ")
+	_, err = fmt.Scanf("%s\n", &password)
+	if err != nil {
+		return "", "", err
+	}
+
+	return username, password, nil
 }
